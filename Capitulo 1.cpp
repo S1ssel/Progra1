5fb8fcdad519d118a6487db0f1e86fdd70b6cae1
S1ssel <39318292+S1ssel@users.noreply.github.com>
2018-09-07 22:37:18 -0500

    Add files via upload

---
+++ Capitulo 1.cpp
@@ -0,0 +1,65 @@
+#include "Includes.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hola mundo\n";
+	cout << "Aqui vamos!\n";
+	return 0;
+}
+
+int baño()
+{
+	cout << "Paso 1: Te levantas\n";
+	cin.ignore();
+	cout << "Paso 2: Caminas un poco a la derecha para llegar a las escaleras\n";
+	cin.ignore();
+	cout << "Paso 3: Subes las escaleras\n";
+	cin.ignore();
+	cout << "Paso 4: Volteas a la derecha\n";
+	cin.ignore();
+	cout << "Paso 5: Caminas hasta el fondo\n";
+	cin.ignore();
+	cout << "Paso 6: Abres la puerta\n";
+	cin.ignore();
+	return 0;
+}
+
+int casa()
+{
+	cout << "Sales de la escuela";
+	cin.ignore();
+	cout << "Caminas por la derecha hasta llegar a la Avenida";
+	cin.ignore();
+	cout << "En la avenida vuelves a dar vuelta a la derecha";
+	cin.ignore();
+	cout << "Sigues caminando hasta llegar a el tercer semaforo que te encuentres";
+	cin.ignore();
+	cout << "Cruzas la calle hacia la izquierda";
+	cin.ignore();
+	cout << "Caminas derecho hasta la segunda calle";
+	cin.ignore();
+	cout << "Das vuelta a la derecha hacia donde esta el hostal";
+	cin.ignore();
+	cout << "Caminas derecho y cruzas la calle";
+	cin.ignore();
+	cout << "Y sigues hasta donde hay una casa con porton blanco con un arbol chico al lado y uno grande del otro tambien tiene un pequeño balcón";
+	return 0;
+}
+
+int receta()
+{
+	cout << "Como hacer una sopa instantanea. :)";
+	cin.ignore();
+	cout << "Levanta la tapa hasta la linea punteada.";
+	cin.ignore();
+	cout << "Llene el vaso con agua hirviendo hasta la linea marcada en el interior.";
+	cin.ignore();
+	cout << "Cierre bien la tapa y deje reposar por 3 minutos.";
+	cin.ignore();
+	cout << "Quite la tapa.";
+	cin.ignore();
+	cout << "Mezcle bien y disfrute.";
+	return 0;
+}
